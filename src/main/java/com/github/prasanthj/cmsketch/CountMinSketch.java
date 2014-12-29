bf02e4ad2639e96a1550b96dbb2a75490f4469aa
Prasanth Jayachandran <j.prasanth.j@gmail.com>
2014-12-29 01:20:36 -0800

    Updated comments in CMSketch source

--- src/main/java/com/github/prasanthj/cmsketch/CountMinSketch.java
+++ src/main/java/com/github/prasanthj/cmsketch/CountMinSketch.java
@@ -127,6 +127,8 @@ public class CountMinSketch {
     // We use the trick mentioned in "Less Hashing, Same Performance: Building a Better Bloom Filter"
     // by Kirsch et.al. From abstract 'only two hash functions are necessary to effectively
     // implement a Bloom filter without any loss in the asymptotic false positive probability'
+    // The paper also proves that the same technique (using just 2 pairwise independent hash functions)
+    // can be used for Count-Min sketch.
 
     // Lets split up 64-bit hashcode into two 32-bit hashcodes and employ the technique mentioned
     // in the above paper
